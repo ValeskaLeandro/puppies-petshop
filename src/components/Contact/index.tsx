e97065eb0c9d574bd32505cdf93ed83cd2e69fd3
valeskaleandro <valeskaleandro@gmail.com>
2023-07-25 15:39:50 -0300

    :sparkles: feat: Create contact form

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,9 +1,32 @@
-import { Container } from "../../styles/styled-global"
+import { Container, Content } from "./styled"
 
 const Contact = () => {
   return(
-    <Container>
-      contato
+    <Container id="contact">
+      <Content>
+        <div className="img">
+          <img src="/img/labrador.png" alt="" />
+        </div>
+        <div className="contact">
+          <h2>Entre em contato.</h2>
+          <p>Estamos disponíveis e prontos para atendê-lo.</p>
+          <div className="input-group">
+            <input type="text" name="name"/>
+            <label htmlFor="name">Nome</label>
+          </div>
+          <div className="input-group">
+            <input type="text" name="email"/>
+            <label htmlFor="email">E-mail</label>
+          </div>
+          <div className="input-group">
+            <textarea name="message"  rows={5}></textarea>
+            <label htmlFor="message">Mensagem</label>
+          </div>
+          <div className="button">
+            <button>Enviar</button>
+          </div>
+        </div>
+      </Content>
     </Container>
   )
 }
--- src/components/Contact/styled.tsx
+++ src/components/Contact/styled.tsx
@@ -1,5 +1,96 @@
 import styled from "styled-components"
 
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 2rem;
+  background-color: var(--pink);
+  padding: 3rem 0;
+`
 export const Content = styled.div`
   width: 70%;
+  display: flex;
+  align-items: center;
+  p {
+    margin-bottom: 1.5rem;
+  }
+  .img, .contact {
+    flex: 1;
+  }
+  .img img {
+    width: 100%;
+    max-height: 500px;
+    object-fit: contain;
+  }
+  .input-group{
+    position: relative;
+  }
+
+  input, textarea {
+    width: 80%;
+    border: 2px solid var(--ligth-green);
+    border-radius: 1rem;
+    background: none;
+    padding: 1rem;
+    transition: .15s cubic-bezier(.4, 0, .2, 1);
+    margin-bottom: 1rem;
+  }
+
+  label {
+    position: absolute;
+    left: 16px;
+    pointer-events: none;
+    transform: translateY(1rem);
+    transition: .15s cubic-bezier(.4, 0, .2, 1)
+  }
+
+  input:focus, 
+  textarea:focus {
+    outline: none;
+    border: 2px solid var(--ligth-green);
+  }
+
+  input:focus ~ label, 
+  textarea:focus ~ label{
+    transform: translateY(-50%) scale(.8);
+    background-color: var(--pink);
+    padding: 0 .2rem;
+    color: var(--ligth-green);
+  }
+
+  .button {
+    width: 80%;
+    display: flex;
+    justify-content: end;
+  }
+
+  .button button {
+    position: relative;
+    font-weight: 600;
+    font-size: 1rem;
+    padding: 1rem 2rem;
+    border: 2px solid var(--ligth-green);
+    border-radius: 1rem;
+    background: transparent;
+    cursor: pointer;
+    transition: .8s ease-in-out;
+
+  }
+  .button button:hover {
+    background-color: var(--ligth-green);
+  }
+
+  @media (max-width: 750px) {
+    justify-content: center;
+    .button {
+      width: 100%;
+    }
+    .img {
+      display: none;
+    }
+    input, textarea {
+    width: 100%;
+    }
+  }
 `
